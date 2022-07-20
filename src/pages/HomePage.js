04a0c92416f3e620c48371955f0e8c4da320afd0
JS1209 <j.a.schoenmakers@outlook.com>
2022-07-20 14:11:22 +0200

    expanded detailspage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,10 +3,11 @@ import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import ProductCard from "../components/ProductCard";
 
-const API_URL = "http://localhost:4000/products";
+const API_URL = "http://localhost:4000/products/all";
 
 export default function HomePage() {
 	const [products, set_products] = useState([]);
+	const [price, set_price] = useState([]);
 
 	const fetchProducts = async () => {
 		const response = await axios.get(API_URL);
@@ -17,13 +18,20 @@ export default function HomePage() {
 		fetchProducts();
 	}, []);
 
+	// if (price != []) {
+	// 	const newList = products.map((product) => {
+	// 		if (product.price > laag&& product.price < hoog) {
+	// 			return product;
+	// 		}
+	// 	})
+	// 	set_products(newList);
+	// }
+
 	return (
 
 		<div className="h-container">
 			<ul className="product-card">
-				{products &&
-
-					products.map((product) => (
+				{products && products.map((product) => (
 						<ProductCard
 							id={product.id}
 							key={product.id}
